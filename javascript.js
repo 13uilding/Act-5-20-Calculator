0464d9ed0eb83c6ab12f0f3602bfa19dee0a0620
deebs <hereisdanbrown@gmail.com>
2019-12-18 10:34:19 -0700

    Made our code a bit prettier and added comments

--- javascript.js
+++ javascript.js
@@ -1,6 +1,12 @@
 var firstNumberFill = true;
 var operatorField = $("#operator");
-
+const math_it_up = {
+  '+': function (x, y) { return x + y },
+  '-': function (x, y) { return x - y },
+  '*': function (x, y) { return x * y },
+  '/': function (x, y) { return x / y },
+  '**': function (x, y) { return x ** y },
+};
 
 $(document).ready(function() {
 
@@ -22,73 +28,45 @@ $(".btn-danger").on("click", function(event){
 
   switch (operator) {
 
-    // Move Buttons (Keyboard Down)
     case "plus":
       operatorField.text("+");
       firstNumberFill = false;
-      operator = "+"
-      console.log(operatorField.text());
       break;
 
-      // Move Buttons (Keyboard Right)
     case "minus":
       operatorField.text("-");
       firstNumberFill = false;
-      operator = "-"
-      console.log(operatorField.text());
       break;
 
-      // Move Buttons (Keyboard Up)
     case "times":
       operatorField.html(`*`);// &times;
       firstNumberFill = false;
-      operator = "*"
-      console.log(operatorField.text());
       break;
 
-      // Move Buttons (Keyboard Left)
     case "divide":
       operatorField.html("/"); // &divide;
       firstNumberFill = false;
-      operator = "/"
-      console.log(operatorField.text());
       break;
 
-      // Move Buttons (Keyboard Left)
     case "power":
       operatorField.text("**"); //^
       firstNumberFill = false;
-      operator = "**";
       break;
-
     }
-})
+});
 
-// Result button  THIS IS WHERE THE PROBLEM IS HAPPENING HENCE THE console.logs
+// Result button
 $(".btn-success").on("click", function(event){
   event.preventDefault();
   var num1 = parseInt($("#first-number").text());
   var num2 = parseInt($("#second-number").text());
-  // console.log(num1)
-  // console.log(typeof num2)
   var operatorFieldText = operatorField.html()
-  // console.log(operatorFieldText);
-  var math_it_up = {
-    '+': function (x, y) { return x + y },
-    '-': function (x, y) { return x - y },
-    '*': function (x, y) { return x * y },
-    '/': function (x, y) { return x / y },
-    '**': function (x, y) { return x ** y },
-  };
-  // console.log(math_it_up[operatorFieldText])
   var numbersFunction = math_it_up[operatorFieldText];
-  // console.log(numbersFunction);
   var result = numbersFunction(num1, num2);
-  // console.log(result);
   $("#result").text(result);
-
 });
 
+// Remove button
 $(".btn-dark").on("click", function(event){
   event.preventDefault();
   $("#first-number").empty();
@@ -96,8 +74,6 @@ $(".btn-dark").on("click", function(event){
   $("#second-number").empty();
   $("#result").empty();
   firstNumberFill = true;
-
-  
-})
+});
 
 });
